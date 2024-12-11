a51bcd4aa2786a6b04c14eeb11e77efbfaff2f45
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-12-11 15:09:06 -0800

    Build dir

---
+++ client/build/scripts/main.js
@@ -0,0 +1,13 @@
+// public/scripts/main.js
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Job Portal JS is loaded!');
+  
+    const members = document.querySelectorAll('.member');
+    members.forEach(member => {
+      member.addEventListener('click', function () {
+        alert(`You clicked on ${this.textContent}`);
+      });
+    });
+  });
+
+  
